ba5e4fd12b7fb808c1de3ba663219a7493072d96
yazhou.miao@nocsys.com.cn <1191178138@qq.com>
2018-07-12 15:31:20 +0800

    update config

--- devtools/mock/app/config.js
+++ devtools/mock/app/config.js
@@ -1,17 +1,4 @@
 let config = {
-  numberOfProjects: 8,
-  numberOfInstances: 100,
-  minNumberOfInstances: 5,
-  numberOfRacks: 5,
-  numberOfSpines: 3,
-  numberOfComputes: 15,
-  resourceUsageDelay: 10000, // value in microseconds
-  maxResourceUsageLength: 2,
-  switchTypeSwitch: 'SWITCH',
-  switchTypeRouter: 'ROUTER',
-  switchRoleMaster: 'MASTER',
-  switchRoleSlave: 'SLAVE',
-  
   // new config
   deviceNumber: 5,
   deviceTypes: ['leaf', 'spine', 'unknown'],
