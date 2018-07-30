b42665b862f9766886194867c3e4a18c91146dbc
xianfu.zhang <286330850@qq.com>
2018-07-30 15:33:07 +0800

    header增加账号信息和告警

--- src/components/mdl-header/ng/header.controller.js
+++ src/components/mdl-header/ng/header.controller.js
@@ -3,8 +3,11 @@ export class headerController{
     return [
       '$log',
       '$scope',
+      '$cookies',
       '$location',
-      'appService'
+      '_',
+      'appService',
+      'loginDataManager'
     ];
   }
 
@@ -14,15 +17,28 @@ export class headerController{
       this.di[value] = args[index];
     });
     this.scope = this.di.$scope;
+    this.CONST_ADMIN_GROUP = this.di.appService.CONST.ADMIN_GROUP;
     this.scope.groups = this.di.appService.CONST.HEADER;
 
     this.scope.location = (url, event) => {
       event && event.stopPropagation();
+      if (url === '/logout') {
+        this.di.loginDataManager.doLogout();
+      }
       this.di.$location.path(url);
     };
+
+    this.init();
   }
 
-
+  init() {
+    this.di.$log.info('mdl-header controller func');
+    this.scope.userConfig = angular.copy(this.scope.groups.user.items);
+    let useraccount = this.di.$cookies.get('useraccount');
+    if (!useraccount || JSON.parse(useraccount).groups.indexOf(this.CONST_ADMIN_GROUP) === -1) {
+      this.scope.userConfig.splice(0, 1);
+    }
+  }
 }
 
 headerController.$inject = headerController.getDI();
