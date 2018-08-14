6f405dbaa134c557a029113f063916a180febdab
xianfu.zhang <286330850@qq.com>
2018-08-14 11:07:01 +0800

    交换机的link状态使用icon表示

--- src/modules/fabric/service/deviceDetail.service.js
+++ src/modules/fabric/service/deviceDetail.service.js
@@ -139,6 +139,7 @@ export class DeviceDetailService {
       {
         'label': this.translate('MODULES.SWITCHES.LINK.COLUMN.STATUS'),
         'field': 'state',
+        'type': 'icon',
         'layout': {'visible': true, 'sortable': true}
       },
       {
