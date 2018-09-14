24bdea014628cbc7a1fb7f4d012ea01ad65e68e6
longshengwang <wanglongshengdf@126.com>
2018-09-14 10:41:18 +0800

    configuration list页面调试

--- src/modules/configuration/ng/configurationList.controller.js
+++ src/modules/configuration/ng/configurationList.controller.js
@@ -10,6 +10,7 @@ export class ConfigurationListController {
       '$filter',
       '$q',
       'appService',
+      'dialogService',
       'configurationDataManager'
     ];
   }
@@ -26,105 +27,56 @@ export class ConfigurationListController {
     this.fileNameInput = new MDCTextField(document.querySelector('#fileName'));
     this.fileNameInput.disabled = true;
 
-    let test_configurations = [
-        {
-          "subject": "snmp:192.168.0.1:1",
-          "class": "org.onosproject.provider.snmp.device.impl.SnmpDeviceConfig",
-          "config": {
-            "ip": "192.168.0.1",
-            "port": 1,
-            "username": "admin",
-            "password": "admin"
-          }
-        },
-        {
-          "subject": "snmp:192.168.0.2:1",
-          "class": "org.onosproject.provider.snmp.device.impl.SnmpDeviceConfig",
-          "config": {
-            "ip": "192.168.0.2",
-            "port": 161,
-            "username": "admin",
-            "password": "admin"
-          }
-        },
-        {
-          "subject": "snmp:192.168.0.3:1",
-          "class": "org.onosproject.provider.snmp.device.impl.SnmpDeviceConfig",
-          "config": {
-            "ip": "192.168.0.3",
-            "port": 1,
-            "username": "test",
-            "password": "test"
-          }
-        },{
-          "subject": "dhcp:192.168.0.1",
-          "class": "org.onosproject.provider.snmp.device.impl.DhcpDeviceConfig",
-          "config": {
-            "ip": "192.168.0.1",
-            "port": 1,
-          }
-        },
-        {
-          "subject": "dhcp:192.168.0.2",
-          "class": "org.onosproject.provider.snmp.device.impl.DhcpDeviceConfig",
-          "config": {
-            "ip": "192.168.0.2",
-            "port": 161,
-          }
-        },
-        {
-          "subject": "dhcp:192.168.0.3",
-          "class": "org.onosproject.provider.snmp.device.impl.DhcpDeviceConfig",
-          "config": {
-            "ip": "192.168.0.3",
-            "port": 1,
-          }
-        }
-      ];
-
     scope.configurationListModel = {
-      subjectClassList: test_configurations,
-      subjectList: [],
-      subjectListDisable: 'true',
-      currentSubjectClass: '',
-      currentSubject: '',
       configurationShow: '',
       isEditable: false
     };
 
     scope.checkDisLab = {id: 'check_edit_config', label: this.translate('MODULES.CONFIGURATION.OPTION.START_CHECK')};
-    scope.fileNameSelectedDisLab = {hint: this.translate('MODULES.CONFIGURATION.FILENAME'),options:[]};
-    // "{hint: '档案名', options: [{label: '男1', value: '0'}, {label: '男2', value: '1'}]}"
+    // scope.fileNameSelectedDisLab = {hint: this.translate('MODULES.CONFIGURATION.FILENAME'),options:[]};
+    scope.fileNameSelectedDisLab = {options:[]};
 
 
     scope.addConfigFile = (evt) => {
-      console.log(this.fileNameInput);
       let filename = this.fileNameInput.value;
-      let config = scope.configurationListModel.configurationShow;
 
-      this.di.configurationDataManager.setConfigurationFile(filename, config).then(
-        (res) => {
-          init();
-        },
-        (err) => {
-          console.log(err)
-        }
-      )
+
+      let preContent = document.getElementById("configurationListPre").innerHTML;
+      let config = JSON.parse(preContent);
+
+
+      this.di.dialogService.createDialog('warning', this.translate('MODULES.CONFIGURATION_LIST.UPDATE.CONFORM'))
+        .then((data)=>{
+          this.di.configurationDataManager.setConfigurationFile(filename, config).then(
+            (res) => {
+              init();
+            },
+            (err) => {
+              console.log(err)
+            }
+          )
+        },(res)=>{
+
+        })
+
+
     };
 
     let getConfigurationList = ()=>{
 
-      let test_options = [{label:"客户诺配置",value:"myasgaggasdg"},{label:"客户云配置",value:"myasgaggasdg1"},{label:"客户Mars配置",value:"myasgaggasdg2"}]
-      scope.fileNameSelectedDisLab.options = test_options;
+      // let test_options = [{label:"客户诺配置",value:"myasgaggasdg"},{label:"客户云配置",value:"myasgaggasdg1"},{label:"客户Mars配置",value:"myasgaggasdg2"}]
+      // scope.fileNameSelectedDisLab.options = test_options;
 
-      // //mock没有数据，暂时注释 //TODO
-      // this.di.configurationDataManager.getConfigurationFileList()
-      //   .then((res)=>{
-      //     // {label: '男1', value: '0'}
-      //     let opts = [];
-      //     //TODO 具体操作，根据返回结果来具体实现
-      //     scope.fileNameSelectedDisLab.options = opts;
-      //   });
+      this.di.configurationDataManager.getConfigurationFileList()
+        .then((res)=>{
+          let opts = [{label:"请选择清单",value:"default"}];
+          // let opts = [];
+          this.di._.forEach(res, (item)=>{
+            opts.push({label:item,value:item})
+          });
+          scope.fileNameSelectedDisLab.options = opts;
+          scope.configurationListModel.fileNameSelected = scope.fileNameSelectedDisLab.options[0];
+        });
 
     };
 
@@ -132,12 +84,15 @@ export class ConfigurationListController {
     let getConfigurationByName = (name) =>{
       //TODO 测试使用
       this.di.$scope.configurationListModel.configurationShow = '';
+      if( name === 'default' || name === '' || name === null|| name === undefined){
+        return ;
+      }
 
-      // this.di.configurationDataManager.getConfigurationByFileName(name)
-      //   .then((res)=>{
-      //     //TODO 具体操作，根据返回结果来具体实现
-      //     this.di.$scope.configurationModel.configurationShow = JSON.stringify(res['data']['config'], null, 2);
-      //   });
+      this.di.configurationDataManager.getConfigurationByFileName(name)
+        .then((res)=>{
+          //TODO 具体操作，根据返回结果来具体实现
+          this.di.$scope.configurationListModel.configurationShow = JSON.stringify(res['data'], null, 4);
+        });
     };
 
     let init = ()=> {
