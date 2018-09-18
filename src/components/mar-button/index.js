48f1770cafb7a9ae10307b5be2ac40cf3eb4ae9f
yazhou.miao@nocsys.com.cn <1191178138@qq.com>
2018-09-18 17:42:40 +0800

    add mar-button component

---
+++ src/components/mar-button/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wls on 2018/6/7.
+ */
+import './scss/mar-button.scss';
+
+import { MarButton } from './ng/marButton.directive';
+
+export default angular
+  .module('marButton', [])
+  .directive('marButton', MarButton)
+  .name;
