b9478b77bec71aab803fce6c7158bd7650122d5d
longshengwang <wanglongshengdf@126.com>
2018-11-05 11:16:13 +0800

    处理openflow交换机的显示

--- src/components/device-tooltip/ng/deviceTooltip.js
+++ src/components/device-tooltip/ng/deviceTooltip.js
@@ -67,6 +67,9 @@ export class deviceTooltip {
           } else if(item.value === "true" || item.value === true) {
             secondTdContent = "<div><svg xmlns='http://www.w3.org/2000/svg' width='18' height='18' viewBox='0 0 18 18'><path fill='none' d='M0 0h18v18H0z'/><path d='M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z' fill='green'/></svg></div>";
           } else {
+              if(item.value === undefined){
+                item.value = ''
+              }
               secondTdContent = '<div>'+ item.value +'</div>';
           }
 
