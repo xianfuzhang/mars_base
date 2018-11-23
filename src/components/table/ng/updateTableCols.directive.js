b3c210c7541e6505e4d907870037ae15ddd02004
xianfu.zhang <286330850@qq.com>
2018-11-23 16:42:29 +0800

    列宽拖动有最小60px保护

--- src/components/table/ng/updateTableCols.directive.js
+++ src/components/table/ng/updateTableCols.directive.js
@@ -1,76 +1,76 @@
 export class updateTableCols {
-	static getDI() {
-		return [
-			'$rootScope',
-			'$timeout',
-			'_'
-		];
-	}
-	constructor(...args) {
-		this.di = {};
+  static getDI() {
+    return [
+      '$rootScope',
+      '$timeout',
+      '_'
+    ];
+  }
+  constructor(...args) {
+    this.di = {};
     updateTableCols.getDI().forEach((value, index) => {
       this.di[value] = args[index];
     });
     this.restrict = 'A';
     this.require = '^mdlTable';
     this.link = (...args) => this._link.apply(this, args);
-	}
+  }
 
-	_link(scope, element, attrs, ctrl) {
-		const CONST_MIN_WIDTH = 60,
-					headerElm = element.children().eq(0), 
-					bodyElm = element.children().eq(1);
-		let unsubscribers = [];
-		let cols = []; // {'resize-col-0': element, 'width:' 100, 'hide': false}
+  _link(scope, element, attrs, ctrl) {
+    const CONST_MIN_WIDTH = 60,
+          headerElm = element.children().eq(0), 
+          bodyElm = element.children().eq(1);
+    let unsubscribers = [];
+    let cols = []; // {'resize-col-0': element, 'width:' 100, 'hide': false}
 
-		//检查thead中是否有th的width小于60px
-		let colsMinInspect = () => {
-			let thElms = headerElm.find('th'),
-					inspect = false;
-			for(let i=0; i< thElms.length; i++) {
-				if (!thElms.eq(i).hasClass('ng-hide') && thElms.eq(i)[0].clientWidth < CONST_MIN_WIDTH) {
-					inspect = true;
-					break;
-				}
-			}
-			return inspect;
-		};
-		//获取thead中所有的th以及对应的width
-		let getColsWidth = () => {
-			let scrollWidth = bodyElm[0].offsetWidth - bodyElm[0].clientWidth;
-			if (!cols.length) {
-				let thElms = headerElm.find('th');
-				for(let i=0; i< thElms.length; i++) {
-					let obj = {};
-					obj['resize-col-' + i] = thElms.eq(i)[0];
-					obj['width'] = thElms.eq(i)[0].clientWidth-2; //table的padding默认是2px
-					obj['hide'] = thElms.eq(i).hasClass('ng-hide');
-					cols.push(obj);
-					//console.log('getColsWidth...index=' + i + ' , width=' + obj['width']);
-				}
-			}
-			//update
-			else {
-				cols.forEach((colObj, index) => {
-					colObj['width'] = colObj['resize-col-' +index].clientWidth-2; //table的padding默认是2px
-					colObj['hide'] = angular.element(colObj['resize-col-' + index]).hasClass('ng-hide');
-					//console.log('getColsWidth...index=' + index + ' , width=' + colObj['width']);
-				});
-			}
-		};
-		//调整tbody中td的width
-		let updateBodyColsWidth = () => {
-			const trElms = bodyElm.find('tr'),
-						tdElms = trElms.eq(0).find('td');
-			if (tdElms.length > 1) {
-				for(let i=0; i< tdElms.length; i++) {
-					if (!cols[i].hide) {
-						tdElms.eq(i)[0].style.width = cols[i].width + 'px';	
-						//console.log('updateBodyColsWidth...index=' + i + ' , width=' + cols[i].width);
-					}
-				}
-			}
-		};
+    //检查thead中是否有th的width小于60px
+    let colsMinInspect = () => {
+      let thElms = headerElm.find('th'),
+          inspect = false;
+      for(let i=0; i< thElms.length; i++) {
+        if (!thElms.eq(i).hasClass('ng-hide') && thElms.eq(i)[0].clientWidth < CONST_MIN_WIDTH) {
+          inspect = true;
+          break;
+        }
+      }
+      return inspect;
+    };
+    //获取thead中所有的th以及对应的width
+    let getColsWidth = () => {
+      let scrollWidth = bodyElm[0].offsetWidth - bodyElm[0].clientWidth;
+      if (!cols.length) {
+        let thElms = headerElm.find('th');
+        for(let i=0; i< thElms.length; i++) {
+          let obj = {};
+          obj['resize-col-' + i] = thElms.eq(i)[0];
+          obj['width'] = thElms.eq(i)[0].clientWidth-2; //table的padding默认是2px
+          obj['hide'] = thElms.eq(i).hasClass('ng-hide');
+          cols.push(obj);
+          //console.log('getColsWidth...index=' + i + ' , width=' + obj['width']);
+        }
+      }
+      //update
+      else {
+        cols.forEach((colObj, index) => {
+          colObj['width'] = colObj['resize-col-' +index].clientWidth-2; //table的padding默认是2px
+          colObj['hide'] = angular.element(colObj['resize-col-' + index]).hasClass('ng-hide');
+          //console.log('getColsWidth...index=' + index + ' , width=' + colObj['width']);
+        });
+      }
+    };
+    //调整tbody中td的width
+    let updateBodyColsWidth = () => {
+      const trElms = bodyElm.find('tr'),
+            tdElms = trElms.eq(0).find('td');
+      if (tdElms.length > 1) {
+        for(let i=0; i< tdElms.length; i++) {
+          if (!cols[i].hide) {
+            tdElms.eq(i)[0].style.width = cols[i].width + 'px'; 
+            //console.log('updateBodyColsWidth...index=' + i + ' , width=' + cols[i].width);
+          }
+        }
+      }
+    };
 
     let dict = {};
     //thead手动调整th col width
@@ -79,15 +79,15 @@ export class updateTableCols {
     }));
 
     //显示隐藏columns时调整table的tds width
-		unsubscribers.push(this.di.$rootScope.$on('table-show-hide-columns', (event) => {
+    unsubscribers.push(this.di.$rootScope.$on('table-show-hide-columns', (event) => {
       getColsWidth();
       updateBodyColsWidth();
     }));
     //tbody渲染以后调整td宽度（scroll宽度影响tds）
-		unsubscribers.push(this.di.$rootScope.$on('table-render-ready', (event) => {
-			getColsWidth();
+    unsubscribers.push(this.di.$rootScope.$on('table-render-ready', (event) => {
+      getColsWidth();
       updateBodyColsWidth();
-		}));  
+    }));  
 
     let mousemoveEvent = (event) => {
       if (dict.startResize) {
@@ -95,16 +95,16 @@ export class updateTableCols {
         let offset = event.clientX - dict.startX;
         //当某col达到最小60px，取消move
         if (colsMinInspect() && offset > 0 ) {
-        	return false;
+          return false;
         }
         let width = dict.width + offset;
         if (width < CONST_MIN_WIDTH) width = CONST_MIN_WIDTH;
         dict.element.style.width = width + 'px';
         
         //统计table header th的width
-				getColsWidth();
-				//修改对应class的width
-				updateBodyColsWidth();
+        getColsWidth();
+        //修改对应class的width
+        updateBodyColsWidth();
       }
     };
     let mouseupEvent = (event) => {
@@ -113,22 +113,22 @@ export class updateTableCols {
       }
     };
     let resizeEvent = (event) => {
-    	getColsWidth();
+      getColsWidth();
       updateBodyColsWidth();
     };
     document.addEventListener('mousemove', mousemoveEvent, false);
     document.addEventListener('mouseup', mouseupEvent, false);
     window.addEventListener('resize', resizeEvent, false);
 
-		scope.$on('$destroy', ()=> {
-			document.removeEventListener('mousemove', mousemoveEvent);
+    scope.$on('$destroy', ()=> {
+      document.removeEventListener('mousemove', mousemoveEvent);
       document.removeEventListener('mouseup', mouseupEvent);
       window.removeEventListener('resize', resizeEvent);
-			unsubscribers.forEach((cb) => {
+      unsubscribers.forEach((cb) => {
         cb();
       });
-		});
-	}
+    });
+  }
 }
 updateTableCols.$inject = updateTableCols.getDI();
 updateTableCols.$$ngIsClass = true;
