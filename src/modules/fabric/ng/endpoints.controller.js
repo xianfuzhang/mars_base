4feaf8d1a351ab56f0beb3b75a683bf0bb168c3b
xianfu.zhang <286330850@qq.com>
2019-02-18 15:44:39 +0800

    主线版本endpoint调用接口变更

--- src/modules/fabric/ng/endpoints.controller.js
+++ src/modules/fabric/ng/endpoints.controller.js
@@ -12,6 +12,7 @@ export class EndPointController {
       'dialogService',
       'notificationService',
       'deviceDataManager',
+      'logicalDataManager',
       'intentDataManager',
       'modalManager',
       'tableProviderFactory'
@@ -125,11 +126,11 @@ export class EndPointController {
     };
 
     this.scope.addEndpoint = () => {
-      let devices = [];
+     /* let devices = [];
       this.di._.forEach(this.scope.deviceObjects, (value, key) => {
         devices.push({'label': value, 'value': key});
-      });
-      if (!devices.length) {
+      });*/
+      if (Object.keys(this.scope.deviceObjects).length === 0) {
         this.scope.alert = {
           type: 'warning',
           msg: this.translate('MODULES.ENDPOINT.CREATE.WARNING.NO_DEVICE')
@@ -137,38 +138,55 @@ export class EndPointController {
         this.di.notificationService.render(this.scope);
         return;
       }
-      this.di.modalManager.open({
+      this.getSegmentsAndDevicesInfo().then((result) => {
+        let segmentArr = [];
+        for(let key in result.segments) {
+          segmentArr.push({
+            'label': key,
+            'value': result.segments[key]
+          });
+        } 
+        this.di.modalManager.open({
           template: require('../components/createEndpoint/template/createEndpoint.html'),
           controller: 'createEndpointCtrl',
           windowClass: 'create-endpoint-modal',
           resolve: {
             dataModel: () => {
               return {
-                devices: devices
+                devices: this.scope.deviceObjects,
+                segments: segmentArr,
+                locations: result.locations
               };
             }
           }
         })
         .result.then((data) => {
-        if (data && !data.canceled) {
-          this.di.deviceDataManager.createEndpoint(data.result).then(
-            () => {
-              this.scope.alert = {
-                type: 'success',
-                msg: this.translate('MODULES.ENDPOINT.CREATE.SUCCESS')
+          if (data && !data.canceled) {
+            this.di.deviceDataManager.createEndpoint(data.result).then(
+              () => {
+                this.scope.alert = {
+                  type: 'success',
+                  msg: this.translate('MODULES.ENDPOINT.CREATE.SUCCESS')
+                }
+                this.di.notificationService.render(this.scope);
+                this.scope.endpointModel.API.queryUpdate();
+              },
+              (msg) => {
+                this.scope.alert = {
+                  type: 'warning',
+                  msg: msg
+                }
+                this.di.notificationService.render(this.scope);
               }
-              this.di.notificationService.render(this.scope);
-              this.scope.endpointModel.API.queryUpdate();
-            },
-            (msg) => {
-              this.scope.alert = {
-                type: 'warning',
-                msg: msg
-              }
-              this.di.notificationService.render(this.scope);
-            }
-          )
+            )
+          }
+        });
+      }, (msg) => {
+        this.scope.alert = {
+          type: 'warning',
+          msg: msg
         }
+        this.di.notificationService.render(this.scope);
       });
     };
 
@@ -229,7 +247,7 @@ export class EndPointController {
       let obj = {};
       obj.id = endpoint.id;
       obj.mac = endpoint.mac;
-      //obj.tenant_name = endpoint.tenant;
+      obj.tenant_name = endpoint.tenant;
       obj.segment_name = endpoint.segment || endpoint.vlan;
       obj.ip = (endpoint.ip_addresses && endpoint.ip_addresses.join(" | ")) 
               || (endpoint.ipAddresses && endpoint.ipAddresses.join(" | "));
@@ -273,6 +291,62 @@ export class EndPointController {
       }
       this.di.notificationService.render(this.scope);
     });
+  }
+
+  getSegmentsAndDevicesInfo() {
+    let resultDefer = this.di.$q.defer(), segmentDefer = this.di.$q.defer();
+    this.di.logicalDataManager.getSegments().then((res) => {
+      let data = res.data.segments.filter((segment) => {
+        return segment.segment_type === 'vlan';
+      });
+      data.length === 0 ? segmentDefer.reject() : segmentDefer.resolve(data);
+    });
+
+    segmentDefer.promise.then((result) => {
+      let deferArr = [];
+      result.forEach((segment) => {
+        let defer = this.di.$q.defer();
+        this.di.logicalDataManager.getSegmentVlanMember(segment.tenant_name, segment.segment_name)
+        .then((res) => {
+          let members = res.data.segment_members.filter((member) => {
+            return member.hasOwnProperty('ports');
+          })
+          members.forEach((member) => {
+            member['tenant_name'] = segment.tenant_name;
+            member['segment_name'] = segment.segment_name;
+          });
+          defer.resolve(members);
+        });
+        deferArr.push(defer.promise);
+      });
+      this.di.$q.all(deferArr).then((arr) => {
+        let segments = {}, switchPorts = {};
+        arr.forEach((item) => {
+          item.forEach((segment) => {
+            let label = segment.tenant_name + '/' + segment.segment_name;
+            if (!segments.hasOwnProperty(label)) {
+              segments[label] = {
+                'tenant': segment.tenant_name,
+                'segment': segment.segment_name
+              }
+            }
+            if (!switchPorts.hasOwnProperty(label)) {
+              switchPorts[label] = {};
+            }
+            if (!switchPorts[label].hasOwnProperty(segment.device_id)) {
+              switchPorts[label][segment.device_id] = segment.ports.map((port) => {
+                return port.split('/')[0];
+              });
+            }
+          });
+        });
+        Object.keys(switchPorts).length === 0 ? resultDefer.reject(this.translate('MODULES.ENDPOINT.CREATE.WARNING.NO_SEGMENT_PORTS')) :
+          resultDefer.resolve({'segments': segments, 'locations': switchPorts});
+      });
+    }, () => {
+      resultDefer.reject(this.translate('MODULES.ENDPOINT.CREATE.WARNING.NO_SEGMENT'));
+    });
+    return resultDefer.promise;
   }
 }
 
