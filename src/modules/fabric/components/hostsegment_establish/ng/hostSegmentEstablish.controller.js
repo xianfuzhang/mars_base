0d4d343631caecf388467c0f6f37de23ea0d2dc9
longshengwang <wanglongshengdf@126.com>
2019-06-06 16:41:52 +0800

    主线host-segment只允许openflow交换机

--- src/modules/fabric/components/hostsegment_establish/ng/hostSegmentEstablish.controller.js
+++ src/modules/fabric/components/hostsegment_establish/ng/hostSegmentEstablish.controller.js
@@ -178,7 +178,8 @@ export class HostSegmentEstablishController {
       };
       deviceDataManager.getDeviceConfigs().then((configs)=>{
         this.di._.forEach(configs,(config)=>{
-          if(config['id'].toLocaleLowerCase().indexOf('grpc') != -1 || config['id'].toLocaleLowerCase().indexOf('rest') != -1){
+          // 顺网只允许openflow，主线目前也只允许openflow
+          if(config['id'].toLocaleLowerCase().indexOf('of') === 0){
             scope.displayLabel.device.options.push({'label':config['name'], 'value':config['id']})
           }
 
