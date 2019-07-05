a57782b225d3dc02c225fb620d9b795a1200dda3
longshengwang <wanglongshengdf@126.com>
2019-07-05 11:15:40 +0800

    1. 默认放开host页面 2. endpoint 移到tenant下面

--- src/apis/services/app.service.js
+++ src/apis/services/app.service.js
@@ -87,8 +87,7 @@ export class appService {
             'items': [
               {'label': this.translate('MODULE.HEADER.FABRIC.SUMMARY'), 'url': '/fabric_summary', 'role': 1},
               {'label': this.translate('MODULE.HEADER.FABRIC.DEVICE'), 'url': '/devices', 'role': 2},
-              {'label': this.translate('MODULE.HEADER.FABRIC.ENDPOINTS'), 'url': '/endpoints', 'role': 2},
-              // {'label': this.translate('MODULE.HEADER.FABRIC.HOSTS'), 'url': '/hosts', 'role': 2},
+              {'label': this.translate('MODULE.HEADER.FABRIC.HOSTS'), 'url': '/hosts', 'role': 2},
               {'label': this.translate('MODULE.HEADER.FABRIC.LOGICAL_PORT'), 'url': '/logical_port', 'role': 3},
               {'label': 'Intents', 'url': '/intents', 'role': 2},
               {'label': 'UpLink', 'url': '/uplinks', 'role': 2},
@@ -119,6 +118,7 @@ export class appService {
             'items':[
               {'label': 'Tenant', 'url': '/tenant', 'role': 3},
               {'label': 'Segment', 'url': '/segment', 'role': 3},
+              {'label': this.translate('MODULE.HEADER.FABRIC.ENDPOINTS'), 'url': '/endpoints', 'role': 2},
               {'label': 'EGP', 'url': '/egp', 'role': 3}
             ]
            },
